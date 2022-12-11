dd0c198fc5395e7af45f561e843187ca08241d5e
David Henry <davhe35@gmail.com>
2022-12-11 18:01:21 +1100

    Refactored roi finding functions.

--- mtf.py
+++ mtf.py
@@ -2,6 +2,7 @@
 Algorithm based on Kao et al. (2005), 
 "A Software tool for measurement of the modulation transfer function"
 """
+from typing import Tuple
 import cv2
 import pydicom
 from scipy.fft import fft, fftfreq
@@ -9,36 +10,29 @@ import numpy as np
 from sklearn.isotonic import IsotonicRegression
 
 
-def rescale_pixels(img, new_max=255, new_min=0):
+def rescale_pixels(image: np.ndarray, new_max: float = 255, new_min: float = 0) -> np.ndarray:
     """
     Rescale pixel values in image.
     """
-    old_max, old_min = img.max(), img.min()
-    new_img = (img-old_min)*((new_max - new_min)/(old_max - old_min)) + new_min
-    return new_img
+    old_max, old_min = image.max(), image.min()
+    new_image = (image-old_min)*((new_max - new_min)/(old_max - old_min)) + new_min
+    return new_image
 
 
-def calculate_distance(v1, v2):
+def calculate_distance(v1: np.array, v2: np.array) -> float:
     """
     Calculates Euclidean distance between two vectors.
     """
     return np.sqrt(np.sum((v1 - v2)**2))
 
 
-def get_roi_bounds(canny):
+def bound_edge_tool(canny: np.ndarray) -> Tuple[int, int, int, int]:
     """
-    Get the indicies for the row and column bounds for edge ROIs.
-
-    Returns a dictionary with index entries for each edge. Each entry is a 
-    a tuple of tuples, with the first tuple being the first and last row
-    indices for the ROI, and the second tuple being the first and last
-    column indices for the ROI. For example:
-    {
-        'left': ((row_start, row_end), (col_start, col_end)),
-        'right': ((row_start, row_end), (col_start, col_end))
-    }
+    Return x, y, w, h, where x, y are the column, row indices of the top left
+    corner of the bounding box around edge tool. w and h are the width and
+    height of the box, respectively
     """
-    contours, hier = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     max_len = 0
     # Assumes the biggest contour is the MTF tool.
     for i, c in enumerate(contours):
@@ -51,7 +45,47 @@ def get_roi_bounds(canny):
     # Define rectangle around the MTF edge.
     # rect returns x,y,w,h where x,y are the column, row indices of top left corner
     # w, h are the number of columns and rows, respectively
-    x, y, w, h = cv2.boundingRect(mtfedge)
+    return cv2.boundingRect(mtfedge)
+
+
+def contourmid2roimid(contour_midpoint: np.array, canny: np.ndarray, edge_location: str, search_length: int) -> np.array:
+    edge_slices = {
+        'left': canny[contour_midpoint[0], contour_midpoint[1]:contour_midpoint[1]+search_length],
+        'right': canny[contour_midpoint[0], contour_midpoint[1]-search_length:contour_midpoint[1]],
+        'top': canny[contour_midpoint[0]:contour_midpoint[0]+search_length, contour_midpoint[1]],
+        'bottom': canny[contour_midpoint[0]-search_length:contour_midpoint[0], contour_midpoint[1]],
+    }
+    slice_start = {
+        'left': np.array([contour_midpoint[0], contour_midpoint[1]]),
+        'right': np.array([contour_midpoint[0], contour_midpoint[1]-search_length]),
+        'top': np.array([contour_midpoint[0], contour_midpoint[1]]),
+        'bottom': np.array([contour_midpoint[0]-search_length, contour_midpoint[1]]),
+    }
+    search_slice = edge_slices[edge_location]
+    local_idx = np.where(search_slice!=0)[0]
+    if local_idx.size > 0:
+        if edge_location in ['left', 'right']:
+            roi_midpoint = slice_start[edge_location] + np.array([0, local_idx[0]])
+        elif edge_location in ['top', 'bottom']:
+            roi_midpoint = slice_start[edge_location] + np.array([local_idx[0], 0])
+
+    return roi_midpoint
+
+
+def get_roi_bounds(canny: np.ndarray) -> dict:
+    """
+    Get the indicies for the row and column bounds for edge ROIs.
+
+    Returns a dictionary with index entries for each edge. Each entry is a 
+    a tuple of tuples, with the first tuple being the first and last row
+    indices for the ROI, and the second tuple being the first and last
+    column indices for the ROI. For example:
+    {
+        'left': ((row_start, row_end), (col_start, col_end)),
+        'right': ((row_start, row_end), (col_start, col_end))
+    }
+    """
+    x, y, w, h = bound_edge_tool(canny)
     # Get midpoint for each side
     contour_midpoints = {
         'left': np.array([y, x]) + np.array([int(h/2), 0]),
@@ -61,12 +95,7 @@ def get_roi_bounds(canny):
     }
     # If the edge is not found within search_length, assumes no edge.
     search_length = 100
-    contour_dirs = {
-        'left': search_length,
-        'right': -search_length,
-        'top': -search_length,
-        'bottom': search_length
-    }
+
     # Define heights for vertical and horizontal edge rois
     height_ver = 0.7*h
     height_hor = 0.8*h
@@ -76,52 +105,23 @@ def get_roi_bounds(canny):
     width_hor = 0.6*w
     
     roi_bounds = {}
-    
+    # Search for edge tool midpoints from contour midpoints.
     for key, val in contour_midpoints.items():
-        if key == 'left':
-            search_slice = canny[val[0], val[1]:val[1]+search_length]
-            local_idx = np.where(search_slice!=0)[0]
-            if local_idx.size > 0:
-                roi_midpoint = np.array([val[0], val[1]+local_idx[0]])
-                roi_bounds[key] = (
-                    (int(roi_midpoint[0]-height_ver/2), int(roi_midpoint[0]+height_ver/2)),
-                    (int(roi_midpoint[1]-width_ver/2), int(roi_midpoint[1]+width_ver/2))
-                )
-            
-        elif key == 'right':
-            search_slice = canny[val[0], val[1]-search_length:val[1]]
-            local_idx = np.where(search_slice!=0)[0]
-            if local_idx.size > 0:
-                roi_midpoint = np.array([val[0], val[1]-search_length+local_idx[0]])
-                roi_bounds[key] = (
-                    (int(roi_midpoint[0]-height_ver/2), int(roi_midpoint[0]+height_ver/2)),
-                    (int(roi_midpoint[1]-width_ver/2), int(roi_midpoint[1]+width_ver/2))
-                )
+        roi_midpoint = contourmid2roimid(val, canny, key, search_length)
+        if key in ['left', 'right']:
+            roi_height, roi_width = height_ver, width_ver
+        elif key in ['top', 'bottom']:
+            roi_height, roi_width = height_hor, width_hor
 
-        elif key == 'top':
-            search_slice = canny[val[0]:val[0]+search_length, val[1]]
-            local_idx = np.where(search_slice!=0)[0]
-            if local_idx.size > 0:
-                roi_midpoint = np.array([val[0]+local_idx[0], val[1]])
-                roi_bounds[key] = (
-                    (int(roi_midpoint[0]-height_hor/2), int(roi_midpoint[0]+height_hor/2)),
-                    (int(roi_midpoint[1]-width_hor/2), int(roi_midpoint[1]+width_hor/2))
-                )
-                
-        elif key == 'bottom':
-            search_slice = canny[val[0]-search_length:val[0], val[1]]
-            local_idx = np.where(search_slice!=0)[0]
-            if local_idx.size > 0:
-                roi_midpoint = np.array([val[0]-search_length+local_idx[0], val[1]])
-                roi_bounds[key] = (
-                    (int(roi_midpoint[0]-height_hor/2), int(roi_midpoint[0]+height_hor/2)),
-                    (int(roi_midpoint[1]-width_hor/2), int(roi_midpoint[1]+width_hor/2))
-                )
+        roi_bounds[key] = (
+            (int(roi_midpoint[0]-roi_height/2), int(roi_midpoint[0]+roi_height/2)),
+            (int(roi_midpoint[1]-roi_width/2), int(roi_midpoint[1]+roi_width/2))
+        )
 
     return roi_bounds
 
 
-def get_rois(image, roi_bounds):
+def get_rois(image: np.ndarray, roi_bounds):
     """
     Get rois from images defined by roi bounds.
     
@@ -205,9 +205,6 @@ def get_esf(roi, roi_canny=None, edge_direction='vertical',
     
     dists_upsampled = dists_horizontal*supersample_factor
     dists_upsampled = np.round(dists_upsampled)/supersample_factor
-    
-    # Re-bin pixel values based on their distance from the edge
-    unique_bins = np.unique(dists_upsampled)
     
     sample_positions = np.linspace(-(num_edge_samples/2-1)/supersample_factor, 
                                    (num_edge_samples/2)/supersample_factor+1/supersample_factor, num_edge_samples)
